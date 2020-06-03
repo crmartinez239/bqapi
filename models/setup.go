34c0659d3abe417195d8dd5dc8a3eecf24b91eb5
Christopher Martinez <crmartinez239@protonmail.com>
2020-06-03 15:48:23 -0400

    Remove debug prints

--- models/setup.go
+++ models/setup.go
@@ -1,22 +1,16 @@
 package models
 
 import (
-	"fmt"
-
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 func SetupModels() *gorm.DB {
-	fmt.Println("trying to print")
 	db, err := gorm.Open("sqlite3", "orders.db")
 
 	if err != nil {
 		panic("Failed to connect to database")
 	}
-
-	fmt.Println("made it pass fail")
 	db.AutoMigrate(&OrderModel{})
-
 	return db
 }
