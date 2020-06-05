e4721283678c6950699b0c628650b16b1a533553
Christopher Martinez <crmartinez239@protonmail.com>
2020-06-05 00:29:41 -0400

    Add isnew to UpdateOrderModel

--- models/order.go
+++ models/order.go
@@ -14,6 +14,7 @@ type CreateOrderModel struct {
 }
 
 type UpdateOrderModel struct {
-	Type string `json:"type"`
-	Name string `json:"name"`
+	Type  string `json:"type"`
+	Name  string `json:"name"`
+	IsNew string `json:"isnew"`
 }
